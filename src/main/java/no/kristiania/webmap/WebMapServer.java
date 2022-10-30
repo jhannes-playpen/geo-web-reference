91b5781da887be4ac385d565e88cf6d3ebffacf1
Johannes Brodwall <johannes@brodwall.com>
2022-10-30 15:39:08 +0100

    Generate HTML from parcel and serve with Jetty

--- src/main/java/no/kristiania/webmap/WebMapServer.java
+++ src/main/java/no/kristiania/webmap/WebMapServer.java
@@ -1,6 +1,9 @@
 package no.kristiania.webmap;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class WebMapServer {
 
@@ -8,7 +11,16 @@ public class WebMapServer {
 
     public WebMapServer(int port) {
         this.server = new Server(port);
+        server.setHandler(createWebApp());
     }
+
+    private Handler createWebApp() {
+        var context = new WebAppContext();
+        context.setContextPath("/");
+        context.setBaseResource(Resource.newClassPathResource("/webapp"));
+        return context;
+    }
+
     private void start() throws Exception {
         server.start();
     }
