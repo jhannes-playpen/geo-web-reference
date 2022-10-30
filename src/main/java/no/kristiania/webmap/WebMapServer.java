3a7d28269cde5f5aad2d907295f68c325298fc98
Johannes Brodwall <johannes@brodwall.com>
2022-10-30 15:47:37 +0100

    Fix hot reloading with Jetty

--- src/main/java/no/kristiania/webmap/WebMapServer.java
+++ src/main/java/no/kristiania/webmap/WebMapServer.java
@@ -3,11 +3,14 @@ package no.kristiania.webmap;
 import org.eclipse.jetty.server.CustomRequestLog;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,17 +19,38 @@ public class WebMapServer {
 
     private final Server server;
 
-    public WebMapServer(int port) {
+    public WebMapServer(int port) throws IOException {
         this.server = new Server(port);
         server.setRequestLog(new CustomRequestLog());
         server.setHandler(createWebApp());
     }
 
-    private Handler createWebApp() {
+    private Handler createWebApp() throws IOException {
         var context = new WebAppContext();
         context.setContextPath("/");
-        context.setBaseResource(Resource.newClassPathResource("/webapp"));
+        var baseResource = Resource.newClassPathResource("/webapp");
+        var sourceResource = getSourceResource(baseResource);
+        if (sourceResource == null) {
+            context.setBaseResource(baseResource);
+        } else {
+            context.setBaseResource(sourceResource);
+            context.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        }
         return context;
+    }
+
+    private Resource getSourceResource(Resource baseResource) throws IOException {
+        var baseDir = baseResource.getFile();
+        if (baseDir == null) {
+            return null;
+        }
+        var sourceDir = new File(baseDir.toString()
+                .replace('\\', '/')
+                .replace("target/classes", "src/main/resources"));
+        if (sourceDir.isDirectory()) {
+            return Resource.newResource(sourceDir);
+        }
+        return null;
     }
 
     private void start() throws Exception {
