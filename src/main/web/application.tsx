507e6c5549166acd4397c3f5939002cebc6052f1
Johannes Brodwall <johannes@brodwall.com>
2022-10-30 15:54:20 +0100

    Add tsconfig and fix ts errors

--- src/main/web/application.tsx
+++ src/main/web/application.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 export function Application() {
     return <h1>Hello React Application</h1>;
 }
--- src/main/web/index.tsx
+++ src/main/web/index.tsx
@@ -1,8 +1,8 @@
+import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {Application} from "./application";
 
 const rootNode = ReactDOM.createRoot(document.getElementById("root")!);
-
 rootNode.render(<Application/>);
 
 
