1643099c3a04e36a21835b6161c3d697a5187a28
Johannes Brodwall <johannes@brodwall.com>
2022-10-30 16:45:50 +0100

    Remember user position

--- src/main/web/components/map/mapView.tsx
+++ src/main/web/components/map/mapView.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { MutableRefObject, useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
 import { Map, View } from "ol";
 import { OSM } from "ol/source";
 import TileLayer from "ol/layer/Tile";
@@ -9,10 +9,21 @@ import { useGeographic } from "ol/proj";
 useGeographic();
 
 export function MapView() {
-  const view = new View({
-    center: [0, 0],
-    zoom: 5,
-  });
+  const [followMe, setFollowMe] = useState(false);
+  const view = useMemo(() => {
+    const sessionViewport = sessionStorage.getItem("viewport");
+    const storageViewport = localStorage.getItem("viewport");
+    const defaultViewport = {
+      center: [9.5, 60],
+      zoom: 5,
+    };
+    const viewport = sessionViewport
+      ? JSON.parse(sessionViewport)
+      : storageViewport
+      ? JSON.parse(storageViewport)
+      : defaultViewport;
+    return new View(viewport);
+  }, []);
   const layers = [
     new TileLayer({
       source: new OSM(),
@@ -20,22 +31,53 @@ export function MapView() {
   ];
 
   useEffect(() => {
-    navigator.geolocation.watchPosition((pos) => {
+    if (!followMe) {
+      return;
+    }
+    const watchId = navigator.geolocation.watchPosition((pos) => {
       console.log(pos);
       const { latitude, longitude } = pos.coords;
       view.setCenter([longitude, latitude]);
-      view.setZoom(9);
     });
-  });
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, [followMe]);
+
+  function handleMoveEnd() {
+    const viewport = {
+      center: view.getCenter(),
+      zoom: view.getZoom(),
+    };
+    localStorage.setItem("viewport", JSON.stringify(viewport));
+    sessionStorage.setItem("viewport", JSON.stringify(viewport));
+    setFollowMe(false);
+  }
 
   return (
     <main>
-      <MapDiv view={view} layers={layers} />
+      <div>
+        <label>
+          Follow me
+          <input
+            type="checkbox"
+            checked={followMe}
+            onChange={(e) => setFollowMe(e.target.checked)}
+          />
+        </label>
+      </div>
+      <MapDiv view={view} layers={layers} onMoveEnd={handleMoveEnd} />
     </main>
   );
 }
 
-export function MapDiv({ view, layers }: { view: View; layers: Layer[] }) {
+export function MapDiv({
+  view,
+  layers,
+  onMoveEnd,
+}: {
+  view: View;
+  layers: Layer[];
+  onMoveEnd(): void;
+}) {
   const mapRef = useRef<HTMLDivElement>() as MutableRefObject<HTMLDivElement>;
 
   useEffect(() => {
@@ -44,6 +86,7 @@ export function MapDiv({ view, layers }: { view: View; layers: Layer[] }) {
     }
     const target = mapRef.current;
     const map = new Map({ layers, target, view });
+    map.on("moveend", onMoveEnd);
     return () => map.setTarget(undefined);
   }, [mapRef.current]);
 
