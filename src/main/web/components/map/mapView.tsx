c9bd0c0da908b723dfc0f3a25876cfef3169b72e
Johannes Brodwall <johannes@brodwall.com>
2022-10-30 16:17:49 +0100

    Extract Map view and layers as props

--- src/main/web/components/map/mapView.tsx
+++ src/main/web/components/map/mapView.tsx
@@ -3,32 +3,37 @@ import { MutableRefObject, useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import { OSM } from "ol/source";
 import TileLayer from "ol/layer/Tile";
+import { Layer } from "ol/layer";
 
 export function MapView() {
+  const view = new View({
+    center: [0, 0],
+    zoom: 5,
+  });
+  const layers = [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ];
+
+  return (
+    <main>
+      <MapDiv view={view} layers={layers} />
+    </main>
+  );
+}
+
+export function MapDiv({ view, layers }: { view: View; layers: Layer[] }) {
   const mapRef = useRef<HTMLDivElement>() as MutableRefObject<HTMLDivElement>;
 
   useEffect(() => {
     if (!mapRef.current) {
       return;
     }
-    const map = new Map({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      target: mapRef.current,
-      view: new View({
-        center: [0, 0],
-        zoom: 3,
-      }),
-    });
+    const target = mapRef.current;
+    const map = new Map({ layers, target, view });
     return () => map.setTarget(undefined);
   }, [mapRef.current]);
 
-  return (
-    <main>
-      <div className={"map"} ref={mapRef} id="mapId" />
-    </main>
-  );
+  return <div className={"map"} ref={mapRef} />;
 }
